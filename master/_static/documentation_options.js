378a9b046011c90d20818d01b5a729f3a6288afb
TRAVIS-CI <ceholden@gmail.com>
2018-06-19 02:20:45 +0000

    Rebuild gh-pages docs on master: db62883

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.6.gd6af7ff',
+    VERSION: '0+untagged.7.gdb62883',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
