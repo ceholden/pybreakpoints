d6af7ffffc930df6f66c10d78411cb2d911389fb
Chris Holden <ceholden@gmail.com>
2018-06-18 21:49:03 -0400

    Switch to user key over deploy keys
    See https://docs.travis-ci.com/user/private-dependencies/#User-Key

--- docs/scripts/deploy-to-gh-pages.sh
+++ docs/scripts/deploy-to-gh-pages.sh
@@ -9,7 +9,6 @@ PACKAGE="pybreakpoints"
 DOCS=$(dirname $(readlink -f $0))/../
 APIDOC="${DOCS}/source/${PACKAGE}"
 
-KEY_FILE=.deploy_key
 SOURCE=_build
 
 REPO=$(git config remote.origin.url)
@@ -35,21 +34,6 @@ if [ "$TRAVIS" == "true" ]; then
     else
         SRC_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
     fi
-
-    # CONFIGURE GIT
-    ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-    ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-    ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-    ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-    openssl aes-256-cbc \
-            -K $ENCRYPTED_KEY \
-            -iv $ENCRYPTED_IV \
-            -in ${KEY_FILE}.enc \
-            -out ${KEY_FILE} \
-            -d
-    chmod 600 ${KEY_FILE}
-    eval `ssh-agent -s`
-    ssh-add ${KEY_FILE}
 
     git config --global user.email $COMMIT_AUTHOR_EMAIL
     git config --global user.name $COMMIT_AUTHOR_NAME
