8e530d56d52adbad0c6f995c3d945fc7e204c445
Chris Holden <ceholden@gmail.com>
2018-10-30 09:28:04 -0400

    Create template for an 'api' import

---
+++ pybreakpoints/api.py
@@ -0,0 +1,4 @@
+""" Common classes, functions, etc. for :py:mod:`pybreakpoints`
+"""
+# TODO: mimic statsmodels.api by importing all the useful features that
+#       might otherwise end up in an __init__ somewhere
