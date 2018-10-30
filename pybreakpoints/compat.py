0410981bd5e4452ce6d6ab068aba7da610c63dc4
Chris Holden <ceholden@gmail.com>
2018-10-30 09:26:20 -0400

    Add compat for dask/cytoolz

--- pybreakpoints/compat.py
+++ pybreakpoints/compat.py
@@ -1,5 +1,6 @@
 """ Various compatibility assists, including hide that Numba is optional
 """
+# Numba
 HAS_JIT = True
 try:
     from numba import jit
@@ -9,3 +10,25 @@ except ImportError:
         def _(func):
             return func
         return _
+
+
+# Dask
+HAS_DASK = True
+try:
+    import dask.array as da
+    import dask.dataframe as ddf
+except ImportError:
+    HAS_DASK = False
+    da, ddf = None, None
+
+
+# Toolz
+HAS_TOOLZ = True
+try:
+    import cytoolz as toolz
+except ImportError:
+    try:
+        import toolz
+    except ImportError:
+        HAS_TOOLZ = False
+        toolz = None
